261d3851adf6115e7286a4da3746abf449ef05c9
Aya yassien <yassienaya714@gmail.com>
2026-05-01 17:20:10 +0300

    Style the login screen

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,9 +1,65 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {useState} from "react";
+import {View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
 
 const AuthScreen = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [isRegistering, setIsRegistering] = useState(false);
+  const [error, setError] = useState('');
+
   return(
     <View style={styles.container}>
-      <Text>Auth Screen</Text>
+  
+      <View style={styles.content}>
+        <Text style={styles.header}>
+          {isRegistering ? 'Sign Up' : 'Login'}
+        </Text>
+
+        {error ? <Text style={styles.error}>{error}</Text> : null}
+
+        <TextInput style={styles.input}
+        placeholder="Email"
+        placeholderTextColor="#aaa"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        keyboardType="email-address"
+        />
+        <TextInput style={styles.input}
+        placeholder="Password"
+        placeholderTextColor="#aaa"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        />
+        {isRegistering && (
+          <TextInput style={styles.input}
+          placeholder="Confirm Password"
+          placeholderTextColor="#aaa"
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+          secureTextEntry
+        />)}
+
+        <TouchableOpacity 
+        style={styles.button}>
+          <Text style={styles.buttonText}>
+            {isRegistering ? 'Sign Up' : 'Login'}
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => setIsRegistering(!isRegistering)}>
+          <Text 
+          style={styles.switchText}
+          adjustsFontSizeToFit={true} 
+          numberOfLines={1}>
+            {isRegistering ? 'Already have an account? Login' : "Don't have an account? Sign Up"}
+          </Text>
+        </TouchableOpacity>
+
+      </View>
     </View>
   );
 };
@@ -11,9 +67,54 @@ const AuthScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
     backgroundColor: '#fff',
-  }
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    transform: [{ translateY: -60 }], // slight upward shift
+  },
+  header:{
+    fontSize: 35,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#333'
+  }, 
+  input: {
+    width: '100%',
+    padding: 12,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 8,
+    marginBottom: 15,
+    borderColor: '#ccc',
+    fontSize: 16,  
+  },
+  button: {
+    backgroundColor: '#007b',
+    paddingVertical: 12,
+    borderRadius: 8,
+    width: '100%',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  buttonText:{
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  switchText: {
+    color: '#007b',
+    marginTop: 15,
+    fontSize: 16,
+  },
+  error: {
+    color: 'red',
+    marginBottom: 10,
+    fontSize: 16,
+  },
 });
 
 export default AuthScreen;
